a2b126ea7329e58e81ce8de2e907c8e4cfd9c328
jrzinkovich <jen042014@gmail.com>
2016-02-09 07:11:17 -0600

    updated via intellij 2

--- src/main/java/com/zinkovich/controller/CustomerController.java
+++ src/main/java/com/zinkovich/controller/CustomerController.java
@@ -31,7 +31,7 @@ public class CustomerController {
         ModelAndView mv = new ModelAndView();
         mv.addObject("customer", new Customer());
         mv.addObject("listOfCustomers", customerService.findAllCustomers());
-        mv.setViewName("customer");
+        mv.setViewName("customer.html");
         return mv;
     }
 
