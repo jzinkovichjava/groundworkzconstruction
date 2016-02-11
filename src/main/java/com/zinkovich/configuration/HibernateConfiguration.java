b158d384bc0e23d42e1e553c078908b50840df68
jrzinkovich <jen042014@gmail.com>
2016-02-10 20:07:34 -0600

    updated via intellij 2

--- src/main/java/com/zinkovich/configuration/HibernateConfiguration.java
+++ src/main/java/com/zinkovich/configuration/HibernateConfiguration.java
@@ -37,13 +37,24 @@ public class HibernateConfiguration {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        String host = "127.0.0.1"; //System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-        String port = "3306";//System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-        String name = "tomcatapp"; //System.getenv("OPENSHIFT_APP_NAME");
-        dataSource.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
-        dataSource.setUsername("adminKDzXrTR");//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
-        dataSource.setPassword("K44a6EqeU5EH");//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
-        return dataSource;
+
+        if(System.getenv("OPENSHIFT_MYSQL_DB_HOST") != null) {
+            String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+            String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+            String name = System.getenv("OPENSHIFT_APP_NAME");
+            dataSource.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
+            dataSource.setUsername(System.getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+            dataSource.setPassword(System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+        } else {
+            String host = "127.0.0.1"; //System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+            String port = "3306";//System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+            String name = "tomcatapp"; //System.getenv("OPENSHIFT_APP_NAME");
+            dataSource.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
+            dataSource.setUsername("adminKDzXrTR");//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+            dataSource.setPassword("K44a6EqeU5EH");//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+        }
+
+            return dataSource;
     }
 
     private Properties hibernateProperties() {
